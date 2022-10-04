d34482e95c37c69208a26096ee649be94634918b
Mehanik <Ivanforsag@list.ru>
2022-10-05 02:52:35 +0300

    01

--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,104 @@ public class Main {
                 matrix[i][0] = 0;
             }
             try {
-                matrix[i][1] = Integer.parseInt(string2.split(" ")[0]);
+                matrix[i][1] = Integer.parseInt(string2.split(" ")[1]);
             } catch (NumberFormatException e) {
                 matrix[i][1] = 0;
             }
         }
-        int count1 = 0;
-        int count2 = 0;
-        int result = 0;
-        while (result < s) {
-            if (matrix[count1][0] < matrix[count2][1]) {
-                result = result + matrix[count1][0];
-                count1++;
-            } else if (matrix[count1][0] > matrix[count2][1]){
-
+        int count1 = 0;                             // порядковый номер резюме в первой стопке
+        int count2 = 0;                             // порядковый номер резюме во второй стопке
+        int result = 0;                             // сумма зарплат из открытых резюме
+        int countResult1 = 0;                              // количество открытых резюме
+        while (result < s) {                            // первый вариант: берем всегда с менбшей зарплатой
+            if ((matrix[count1][0] < matrix[count2][1])) {
+                if (matrix[count1][0] == 0) {
+                    if (result + matrix[count2][1] > s) break;
+                    result = result + matrix[count2][1];
+                    count2++;
+                    countResult1++;
+                } else {
+                    if (result + matrix[count1][0] > s) break;
+                    result = result + matrix[count1][0];
+                    count1++;
+                    countResult1++;
+                }
+            } else {
+                if (matrix[count2][1] != 0) {
+                    if (result + matrix[count2][1] > s) break;
+                    result = result + matrix[count2][1];
+                    count2++;
+                    countResult1++;
+                } else {
+                    if (result + matrix[count1][0] > s) break;
+                    result = result + matrix[count1][0];
+                    count1++;
+                    countResult1++;
+                }
             }
         }
+        count1 = 0;                             // порядковый номер резюме в первой стопке
+        count2 = 0;                             // порядковый номер резюме во второй стопке
+        result = 0;                             // сумма зарплат из открытых резюме
+        int countResult2 = 0;                   // количество открытых резюме
+        while (result < s) {                    //второй вариант: берем всегда с первой стопки, после со второй
+            if (count1 < matrix.length) {
+                if (matrix[count1][0] != 0) {
+                    if (result + matrix[count1][0] > s) break;
+                    result = result + matrix[count1][0];
+                    count1++;
+                    countResult2++;
+                } else if (count2 < matrix.length) {
+                    if (matrix[count2][1] != 0) {
+                        if (result + matrix[count2][1] > s) break;
+                        result = result + matrix[count2][1];
+                        count2++;
+                        countResult2++;
+                    } else {
+                        break;
+                    }
+                } else break;
+            } else if (count2 < matrix.length) {
+                if (matrix[count2][1] != 0) {
+                    if (result + matrix[count2][1] > s) break;
+                    result = result + matrix[count2][1];
+                    count2++;
+                    countResult2++;
+                }
+            }
+        }
+
+        count1 = 0;                             // порядковый номер резюме в первой стопке
+        count2 = 0;                             // порядковый номер резюме во второй стопке
+        result = 0;                             // сумма зарплат из открытых резюме
+        int countResult3 = 0;                   // количество открытых резюме
+        while (result < s) {                  //третий вариант: берем всегда со второй стопки, затем с первой
+            if (count2 < matrix.length) {
+                if (matrix[count2][1] != 0) {
+                    if (result + matrix[count2][1] > s) break;
+                    result = result + matrix[count2][1];
+                    count2++;
+                    countResult3++;
+                } else if (count1 < matrix.length) {
+                    if (matrix[count1][0] != 0) {
+                        if (result + matrix[count1][0] > s) break;
+                        result = result + matrix[count1][0];
+                        count1++;
+                        countResult3++;
+                    } else {
+                        break;
+                    }
+                } else break;
+            } else if (count1 < matrix.length) {
+                if (matrix[count1][0] != 0) {
+                    if (result + matrix[count1][0] > s) break;
+                    result = result + matrix[count1][0];
+                    count1++;
+                    countResult3++;
+                }
+            }
+        }
+        System.out.println(Math.max(countResult1, Math.max(countResult2, countResult3)));
 
     }
 }
